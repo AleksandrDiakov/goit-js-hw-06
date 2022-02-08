2424e37ccb2b948851a41598bc73734fc4bbe200
Aleksandr Diakov <aldiakov@gmail.com>
2022-02-08 23:52:21 +0200

    fix

--- js/task-08.js
+++ js/task-08.js
@@ -36,27 +36,3 @@ function handleSubmit(event) {
   console.log(formEl);
   event.currentTarget.reset();
 }
-
-
-// const formRef = document.querySelector('.login-form');
-
-// formRef.addEventListener('submit', onFormSubmit);
-
-// function onFormSubmit(event) {
-//     const email = event.currentTarget.elements.email.value;
-//     const password = event.currentTarget.elements.password.value;
-//     event.preventDefault();
-     
-//     if (email === '' || password === '') {
-//         alert('Please note that all fields are required!')
-//     } else {
-//         const formData = {
-//         email,
-//         password,
-//     }
-//         console.log(formData);
-        
-//     }
-    
-//     formRef.reset()
-// }
